9b9e78ed243dc672378c9e0a3011e04fbf911af3
Your Name <you@example.com>
2017-07-19 10:56:01 -0400

    clean code

--- main.js
+++ main.js
@@ -3,23 +3,12 @@ let button=document.querySelector("#foodButton");
 let main = document.querySelector(".container");
 button.addEventListener("click", fetchData);
 function fetchData(){
-  // fetch(`http://www.recipepuppy.com/api/?q=${searchTerm.value}`)
   fetch("http://crossorigin.me/http://www.recipepuppy.com/api/?q="+searchTerm.value)
   .then( function(response){
     if(response.status !== 200){
       console.log(response.status);
       return;
     }
-    // response.json().then(function(data){
-    //   main.innerHTML+=`
-    //   data.results.forEach(results){
-    //     <div class="recipe-box">
-    //       <img src="${results.thumbnail}" alt="">
-    //       <h2>${results.title}</h2>
-    //       <a href="${results.href}"></a>
-    //     </div>
-    //   `;
-
       response.json().then(function(reponse){
 
         response.results.forEach(function(result){
@@ -34,11 +23,3 @@ function fetchData(){
 
   })
 }
-
-
-
-// create an event listener that takes the input value as an argument and calls the function fetchData.
-
- // fetchData puts the data in an api format and gets the data.
- // it goes into the results obj in the data and interates through each obj.
- // It takes each obj retrieves the requested data and displays the data through the template.
